8f12829cedc2e2999469d611620b9152eda85c16
Anil <mohananiltribute@gmail.com>
2022-06-09 14:54:11 +0100

    First commit

---
+++ src/main/java/com/example/demo/ResourceClass.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceClass {
+
+	@GetMapping(value = "/getInfo")
+	public String getData() {
+		return "Enjoy the Docker Jenkins !!!!!!";
+	}
+}
