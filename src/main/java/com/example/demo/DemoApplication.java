43408a1ba732eba30d5751d4d56d1f419b62ed7e
javaguruanil <34855423+javaguruanil@users.noreply.github.com>
2022-06-10 21:00:54 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication //It does internally @SpringBootConfiguration, @EnableConfiguration , @ComponentScan
+@SpringBootApplication 
+//It does internally @SpringBootConfiguration, @EnableConfiguration , @ComponentScan
 public class DemoApplication {
         void m1(){}
 	public static void main(String[] args) {
